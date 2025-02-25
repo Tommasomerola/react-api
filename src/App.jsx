487d6ef24a1724d354cac0db4b4de5adcc59f107
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-25 17:00:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import PostForm from "./components/PostForm"
+import Header from "./components/Header"
+
+function App() {
+
+  
+
+  return (
+    <>
+    
+      <Header/>
+      <PostForm/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+    <h1>questo è il form </h1>
+    )
+}
